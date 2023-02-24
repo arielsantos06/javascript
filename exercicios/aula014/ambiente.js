cadf658b3891728883113dfb9808b2d80e7ebc3c
Ariel dos Santos <arieldossantos2000@outlook.com>
2023-02-24 15:43:05 -0300

    criação de ambiente.js

---
+++ exercicios/aula014/ambiente.js
@@ -0,0 +1,16 @@
+/*
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 3)
+*/
+
+//------------------------
+
+
+console.log('Vai começar...')
+for(var c = 1;c <= 3; c++){
+    console.log(c)
+}
+console.log('FIM!')
