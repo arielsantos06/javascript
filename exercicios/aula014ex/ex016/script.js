75db8a2534ba2d32566f4a17310a20cb29b73eb0
Ariel dos Santos <arieldossantos2000@outlook.com>
2023-02-27 16:44:40 -0300

    aula 14

--- exercicios/aula014ex/ex016/script.js
+++ exercicios/aula014ex/ex016/script.js
@@ -1,6 +1,19 @@
-var n1 = 2
-var n2 = 2
-var n3 = 10
-for(var n1;n1 <= n2; n1 += n3){
-    console.log = `${n1}`
+function contar(){
+    let ini =  window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
+    
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = '[ERRO] Faltam dados!'
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(txt.value)
+        let f = Number(txtf.value)
+        let p = Number(txtp.value)
+
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c}`
+        }
+    }
 }
