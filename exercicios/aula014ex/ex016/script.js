6faaf5c000f035600e2545289374e8cb9833e24f
Ariel dos Santos <arieldossantos2000@outlook.com>
2023-02-27 17:07:40 -0300

    Update script.js

--- exercicios/aula014ex/ex016/script.js
+++ exercicios/aula014ex/ex016/script.js
@@ -5,15 +5,27 @@ function contar(){
     let res = window.document.getElementById('res')
     
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        res.innerHTML = '[ERRO] Faltam dados!'
+        res.innerHTML = 'Impossível contar!'
     } else {
-        res.innerHTML = 'Contando: '
-        let i = Number(txt.value)
-        let f = Number(txtf.value)
-        let p = Number(txtp.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c}`
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo inválido! Reajustando pra 1')
+            p = 1
         }
+        if(i < f){
+            for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+            
+        } else {
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+        
     }
 }
